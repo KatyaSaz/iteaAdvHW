2015ef7d029902739419ec3805538495cbc69cbb
Katya Sazonova <45514862+KatyaSaz@users.noreply.github.com>
2020-06-11 15:40:03 +0300

    first commit of FileManager

---
+++ iteaHW02/src/iteaHW02/FileManagerMain.java
@@ -0,0 +1,8 @@
+package iteaHW02;
+
+public class FileManagerMain {
+	public static void main(String[] args) {
+		FileManager fileManager = new FileManager("C:\\TestLesson02\\komp_seti");
+		fileManager.start();
+	}
+}
